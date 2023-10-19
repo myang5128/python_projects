879761fb21ceab72d70dd1560dc6a65771347f80
Mike <mikeyang5128@gmail.com>
2023-10-19 13:30:25 -0400

    added day 1

---
+++ Band Name Gen.py
@@ -0,0 +1,12 @@
+# greeting
+print("Welcome to the band name generator 100!")
+
+# ask for hometown 
+hometown_Name = input("What's the name of the town you grew up in?\n")
+
+# ask for pet name
+pet_Name = input("If you had a pet, what would you name it?\n")
+
+# combine hometown + pet name
+print("Thinking, thinking.\n")
+print("Your band name shall be: " + hometown_Name + " " + pet_Name + "!")
