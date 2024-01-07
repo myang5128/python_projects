a8498124acec81e6d05e4e58b4bcf09c30c9c0dc
Mike <mikeyang5128@gmail.com>
2024-01-07 14:24:08 -0500

    revised list code

--- CSV and Pandas/US States Quiz/main.py
+++ CSV and Pandas/US States Quiz/main.py
@@ -36,10 +36,7 @@ while len(guessed_states) != 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Correct", prompt="What's a state's name?").title()
     print(answer_state)
     if answer_state == "Exit":
-        missing_states = []
-        for state in state_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [states for states in guessed_states if states not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv(r"CSV and Pandas\US States Quiz\states_to_learn.csv")
         break
